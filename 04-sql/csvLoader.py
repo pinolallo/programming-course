891e2536a8a8f72baa83e42d9a471576cd622c31
sdipietr <silvestro.dipietro@ifom.eu>
2023-06-09 12:50:30 +0200

    lesson session

--- 04-sql/csvLoader.py
+++ 04-sql/csvLoader.py
@@ -4,6 +4,9 @@ sys.path.append('../00-commons')
 import common
 import re
 import constants
+import json
+import  mysql.connector
+
 
 def getDataset():
      with open('datasets/movie_dataset.csv') as movie_csv:
@@ -22,121 +25,102 @@ def getDataset():
           result['db']=rawDb     
           return result
      
-def importSqlGeneator(header,row,databaseDef,mainRecordId):
-     sqlelements=[];
+def importSqlGeneator(header,row,mainRecordId,sqlelements):
      i=0
-     #common.emit(databaseDef)
-     for field in row:
-          #common.emit(f"field {field}")
-          sqlelement={}
+     for fieldValue in row:
           try:
-               sqlelement[header[i]]=fieldValuate(header[i],field,databaseDef,mainRecordId)
+               sqlelements=fieldValuate(header[i],fieldValue,mainRecordId,sqlelements)
           except:
                vvival=''
-               #common.emit(f"fail field evalutate {header[i]} fieldvalue:{field} main recordid:{mainRecordId}")
-          '''try:
-               sqlelement[header[i]]=fieldValuate(header[i],field,databaseDef)
-          except:
-               common.emit(f"fail generating insert sql line for field ({header[i]})",constants.PRINT_MESSAGE+constants.LOG_TO_SYSLOG)'''
-          sqlelements.append(sqlelement)
           i=i+1
+    
+     sqlFields=",".join(sqlelements['queryElement']['fields'])
+     sqlValues=",".join(sqlelements['queryElement']['values'])
+     #print(sqlelements['extraQuery'])
+     recordSql=f'insert IGNORE into {constants.MAIN_DB_TABLE} ({sqlFields}) values ({sqlValues})'
+     sqlelements['extraQuery'].append(recordSql)
+     #print(recordSql)
+     return sqlelements
 
-def fieldValuate(field, value, databaseDef,mainRecordId):
-     hasQuote=True
-     sql=[]
-     result={}
-     result[constants.MAIN_DB_TABLE]={}
-     result[constants.MAIN_DB_TABLE]['field']=[]
-     result[constants.MAIN_DB_TABLE]['value']=[]
+    
+
+
+def fieldValuate(fieldName, fieldValue,mainRecordId,result):
      conversionDictionary={
-          'index':{'action':'null'},
-          'budget':{'action':',fill(movie_budget)'},
-          'genres':{'action':',fill(movie_genres)'},
-          'homepage':{'action':',fill(movie_HomePage)'},
-          'id':{'action':',fill(movie_id)'},
-          'keywords':{'action':',fill(movie_keywords)'},
-          'original_language':{'action':',fill(movie_original_lang)'},
-          'original_title':{'action':',fill(movie_original_title)'},
-          'overview':{'action':',fill(movie_overview)',},
-          'popularity':{'action':',fill(movie_popularity)'},
-          'production_companies':{'action':',putRelation(movie_id,productions,movie_productions)'},
-          'production_countries':{'action':',putRelation(movie_id,countries,movie_countries)'},
-          'release_date':{'action':',fill(movie_relase)'},
-          'revenue':{'action':',fill(movie_revenue)'},
-          'runtime':{'action':',fill(movie_runtime)'},
-          'spoken_languages':{'action':',putRelation(movie_id,laguages,movie_lang)'},
-          'status':{'action':'null'},
-          'tagline':{'action':',fill(movie_tagline)'},
-          'title':{'action':',fill(movie_title)'},
-          'vote_average':{'action':',fill(movie_vote_average)'},
-          'vote_count':{'action':',fill(movie_vote_num)'},
-          'cast':{'action':',fill(movie_cast)'},
-          'crew':{'action':',putDb(credits,job:job_name,department:job_department,name:people_name,gender:people_gender)'},
-          'director':{'action':'null'}
-     }
-     command=conversionDictionary[field]
+               'index':'null',
+               'budget':'fill(movie_budget)',
+               'genres':'fill(movie_genres)',
+               'homepage':'fill(movie_HomePage)',
+               'id':'fill(movie_id)',
+               'keywords':'fill(movie_keywords)',
+               'original_language':'fill(movie_original_lang)',
+               'original_title':'fill(movie_original_title)',
+               'overview':'fill(movie_overview)',
+               'popularity':'fill(movie_popularity)',
+               'production_companies':'putRelation(movie_id,productions,movie_productions)',
+               'production_countries':'putRelation(movie_id,countries,movie_countries)',
+               'release_date':'fill(movie_relase)',
+               'revenue':'fill(movie_revenue)',
+               'runtime':'fill(movie_runtime)',
+               'spoken_languages':'putRelation(movie_id,laguages,movie_lang)',
+               'status':'null',
+               'tagline':'fill(movie_tagline)',
+               'title':'fill(movie_title)',
+               'vote_average':'fill(movie_vote_average)',
+               'vote_count':'fill(movie_vote_num)',
+               'cast':'fill(movie_cast)',
+               'crew':'putDb(credits,job:job_name,department:job_department,name:people_name)',
+               'director':'null'
+          }
    
-     if  command['action'] == 'null':
+     command=conversionDictionary[fieldName]   
+     if  command == 'null':
           return result
-     #fieldType=databaseDef[constants.MAIN_DB_TABLE][field]['fieldType']
-     '''if fieldType=='int' or fieldType=='float':
-          hasQuote=0
-     else:
-          hasQuote=1'''
-     
-     common.emit(f"adadadadadad {command['fieldType']}")
-     parsedFunctions=re.findall(",(.*?)\((.*?)\)",command['action'])
-     
-     common.emit(parsedFunctions)
-
-     for parsedFunction in parsedFunctions:
-          i=0
-          for functionElement in parsedFunction:
-               if i==0:
-                    functionName=functionElement
-               else:
-                    functionArgument=functionElement
-               i=i+1
-          if functionName=='fill':
-               common.emit(f'datasetField:{field} fill dbfield ({functionArgument})')
-               result['field'].append(field)
-               if hasQuote==1:
-                    result[constants.MAIN_DB_TABLE]['value'].append(f"'{value}'")
-               else:
-                    result[constants.MAIN_DB_TABLE]['value'].append(value)
-          elif functionName=='putRelation':
-               result[splittedArgs[2]]={}
-               splittedArgs=functionArgument.split(",")
-               common.emit(f'datasetField:{field} will make a relation in table {splittedArgs[2]} using id ({splittedArgs[0]}) left joined ({splittedArgs[1]})) ')
-          elif functionName=='putDb':
-               common.emit('putDB')
-               splittedArgs=functionArgument.split(",")
-               #popping out the first argument (the table)
-               fillingTable=splittedArgs.pop(0)
-               queryElement={}
-               queryElement['field']=[]
-               queryElement['value']=[]
-               #adding movie id
-               queryElement['field'].append('credit_id')
-               queryElement['value'].append(mainRecordId)
+     match=re.search("(.*?)\((.*?)\)",command)
+     functionName=match.group(1)
+     functionArgument=match.group(2)
+     if functionName=='fill':
+          if not re.match("^[0-9.]+$",fieldValue):
+               fieldValue=fieldValue.replace("'","\'")
+               db_value=(f'"{fieldValue}"')
+          result['queryElement']['values'].append(f'"{fieldValue}"')
+          result['queryElement']['fields'].append(functionArgument)
+          return result
+     elif functionName=='putRelation':
+         # result[splittedArgs[2]]={}
+          #splittedArgs=functionArgument.split(",")
+          common.emit(f'datasetField:{fieldName} will make a relation in table {splittedArgs[2]} using id ({splittedArgs[0]}) left joined ({splittedArgs[1]})) ')
+     elif functionName=='putDb':
+          fieldValue=json.loads(fieldValue.replace("'", '"'))
+          splittedArgs=functionArgument.split(",")
+          #popping out the first argument (the table)
+          fillingTable=splittedArgs.pop(0)
+          #preparing the insert element list struct
+          fieldsFiller={}
+          for recordLine in fieldValue:
+               mfields=[]
+               mvalues=[]
+               #adding relation id to the element list before read the recipe
+               fieldsFiller[constants.MAIN_DB_RECORD_FIELD]=mainRecordId
+               #fill all field name value using the arguments
                for fieldArg in splittedArgs:
-                    #field has colon as sepator
+                    #field has colon as separator, we need the json key associated to 
                     tmp=fieldArg.split(":")
-                    queryElement=putDb(value,tmp[0],tmp[1],queryElement)
-                    common.emit(f'datasetField:{field} will add into db {fillingTable} fieldJason ({tmp[0]}) int dbField ({tmp[1]})) ')
-               for fieldInsert in queryElement:
-                    fields=",".join(fieldInsert['field'])
-                    values=",".join(fieldInsert['value'])
-                    sql.append(f'insert IGNORE into {fillingTable} ({fields}) values ({values})') 
-               common.emit(sql)
+                    fieldsFiller[tmp[1]]=recordLine[tmp[0]]
+               sqlValues=''
+               sqlFields=''
+               try:
+                    for db_field,db_value in fieldsFiller.items():
+                         if not re.match("^[0-9.]+$",db_value):
+                              db_value=(f"'{db_value}'")
+                         mfields.append(f"{db_field}")
+                         mvalues.append(f"{db_value}")
+               except:
+                   #strange error on the fieldFiller
+                   val=""
+               sqlFields=",".join(mfields)
+               sqlValues=",".join(mvalues)
+               common.emit(f'insert IGNORE into {fillingTable} ({sqlFields}) values ({sqlValues});')
+               result['extraQuery'].append(f'insert IGNORE into {fillingTable} ({sqlFields};) values ({sqlValues})')
      return result
 
-
-def putDb(crewSet,fieldJason,dbField,queryElement):
-     queryElement={}
-     queryElement['field']=[]
-     queryElement['value']=[]
-     for crew in crewSet:
-          queryElement['field'].append(dbField)
-          queryElement['value'].append(f"'{crew[fieldJason]}'")
-     return queryElement
